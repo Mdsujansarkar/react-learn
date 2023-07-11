7f0439eee43010a11a8130c326348f05080f25a7
Mdsujansarkar <sujansarkar786@gmail.com>
2023-07-11 22:15:01 +0600

    function add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { useState } from "react";
 
 function App() {
+  let [num,setNum] = useState(0);
+
+  let increMemt =()=>{
+    if(num => 0){
+      setNum(num + 1);
+    }
+  }
+  let decreMemt = () =>{
+    if(num => 0){
+    setNum(num -1 );
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <React.Fragment>
+    <button className="btn btn-outline-primary" onClick={increMemt}>+</button>
+    {num}
+    <button onClick={decreMemt}>-</button>
+    </React.Fragment>
   );
 }
 
